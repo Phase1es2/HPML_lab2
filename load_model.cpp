3a7dfd6d8c6cbc80c15bf5dbfd6f817d0bce98f7
Phase1es2 <hyang17@uw.edu>
2026-03-01 17:51:43 -0500

    fix cpp problem and change the path for CMake

--- load_model.cpp
+++ load_model.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, const char* argv[]) {
     if (argc != 2) {
-        std::cerr << "usage: example-app <path-to-exported-script-module>\n";
+        std::cerr << "usage: my_torch_app <path-to-exported-script-module>\n";
         return -1;
     }
 
-    torch::jit::script::Module modul;
+    torch::jit::script::Module module;
     try {
         module = torch::jit::load(argv[1]);
     } catch (const c10::Error& e) {
@@ -21,7 +21,7 @@ int main(int argc, const char* argv[]) {
     inputs.push_back(torch::ones({1, 3, 32, 32}));
 
     at::Tensor output = module.forward(inputs).toTensor();
+    std::cout << output.slice(1, 0, 5) << "\n";
 
-    std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << "\n";
     return 0;
 }
